bafc01bb1b5b0f3c99cd5498ff70369c8b6fb182
Danielle Martinoli <daniechris5@gmail.com>
2019-04-29 19:58:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var katzDeliLine = [];
 
-function takeANumber(katzDeliLine, name) {
-    katzDeliLine.push(name)
-    return 'Welcome, ' + name + '.' + ' You are number ' + katzDeliLine.length + ' in line.'
+function takeANumber(katzDeliLine, number) {
+    katzDeliLine.push(number)
+    return `Welcome, You are' ${number}.`
 }
 
 function currentLine(n) {
